2b12353cfc894ec6eae539d734af322da2c21117
Marcos Melo <veoeasyoficial@gmail.com>
2025-12-02 21:40:19 -0300

    fix: use relative import for SupportButton to resolve Netlify build error

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
-import { SupportButton } from '@/components/whatsapp/SupportButton'
+import { SupportButton } from '../components/whatsapp/SupportButton'
 
 const inter = Inter({ subsets: ['latin'] })
 
